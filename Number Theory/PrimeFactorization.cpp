835726d0632d0b2e4fa1b0ac65e6fc57cb378193
s-rifat <47311938+s-rifat@users.noreply.github.com>
2020-07-16 18:35:08 +0600

    Update PrimeFactorization.cpp

--- Number Theory/PrimeFactorization.cpp
+++ Number Theory/PrimeFactorization.cpp
@@ -18,27 +18,26 @@ vector<ll> prime,P,E;
 
 void sieve(ll N)
 {
-     ll i, j, sqrtN;
-     sqrtN = ll( sqrt( N ) );
-     for( i = 3; i <= sqrtN; i += 2 )
-     {
+        ll i, j, sqrtN;
+        sqrtN = ll( sqrt( N ) );
+	for( i = 3; i <= sqrtN; i += 2 )
+	{
+		if( Check(status[i/32],i%32)==0)
+		{
+			for( j = i*i; j <= N; j += 2*i )
+			{
+				status[j/32]=Set(status[j/32],j % 32);
+			}
+		}
+	}
+	prime.push_back(2);
+	for(i=3;i<=N;i+=2)
+	{
 		 if( Check(status[i/32],i%32)==0)
 		 {
-	 		 for( j = i*i; j <= N; j += 2*i )
-			 {
-				 status[j/32]=Set(status[j/32],j % 32);
-	 		 }
+			prime.push_back(i);
 		 }
-	 }
-
-	 prime.push_back(2);
-	 for(i=3;i<=N;i+=2)
-     {
-		 if( Check(status[i/32],i%32)==0)
-         {
-	 	    prime.push_back(i);
-         }
-     }
+	}
 }
 
 bool isPrime(ll a)
