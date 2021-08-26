e03335de91090a700771008ce9d2b791eb83f1af
Syed Rifat <gtr06.rft@gmail.com>
2021-08-26 16:45:42 +0600

    Update Histogram_technique.cpp

--- Greedy/Histogram_technique.cpp
+++ Greedy/Histogram_technique.cpp
@@ -1,86 +1,50 @@
 #include<bits/stdc++.h>
 
 using namespace std;
-
-int main()
-
-{
-
-    int t;
-
-    cin>>t;
-
-    int cnt2 = 0;
-    while(t--)
-
-    {
-        cnt2++;
-        int n;
-
-        cin>>n;
-
-        int arr[n];
-
-        for(int i=0;i<n;i++)
+ int largestRectangleArea(vector<int>& heights) {
+        int n = heights.size();
+        stack<int> s;
+        int ans = 0;
+        for(int i=0;i<n;)
         {
-            cin>>arr[i];
-        }
-
-        stack <int> s;
-
-       int area;
-       int Max = -1;
-       int top;
-       int i;
-       for( i=0;i<n;)
-        {
-
-
-            if( s.empty() || arr[i]>=arr[s.top()] )
+            if(s.empty() || heights[i]>heights[s.top()])
             {
                 s.push(i);
                 i++;
             }
             else
             {
-                    top = s.top();
-                    s.pop();
-
-                    if(s.empty())
-
-                        area = arr[top]*i;
-
-                    else
-
-                        area = arr[top]*(i-s.top()-1);
-
-                    if(area>Max)
-                            Max = area;
+                int top = s.top();
+                s.pop();
+                if(s.empty())
+                {
+                   ans = max(ans,heights[top]*i);
+                }
+                else
+                {
+                    ans = max(ans,heights[top]*(i-s.top()-1));
+                }
             }
-
-
         }
-
         while(!s.empty())
         {
-                    top = s.top();
-                    s.pop();
-
-                    if(s.empty())
-
-                        area = arr[top]*i;
-
-                    else
-
-                        area = arr[top]*(i-s.top()-1);
-                    if(area>Max)
-                        Max = area;
+                int top = s.top();
+                s.pop();
+                if(s.empty())
+                {
+                   ans = max(ans,heights[top]*n);
+                }
+                else
+                {
+                    ans = max(ans,heights[top]*(n-s.top()-1));
+                }
         }
-
-          cout<<"Case "<<cnt2<<": "<<Max<<endl;
-
-
+        return ans;
     }
+int main()
+
+{
+
 
     return 0;
 }
