a76703972a1e3cab306fdaca7e426164f5da5598
Syed Rifat <gtr06.rft@gmail.com>
2021-08-26 17:04:15 +0600

    Update kmp.cpp

--- string/kmp.cpp
+++ string/kmp.cpp
@@ -5,20 +5,16 @@ vector<int> lpsArray(string pattern) {
     vector<int> lps(pattern.length());
     int index = 0;
     for (int i = 1; i < pattern.length(); ) {
-        if (pattern[i] == pattern[index])
-             lps[i] = index + 1, index++, i++;
+        if (pattern[i] == pattern[index]) lps[i] = index + 1, index++, i++;
         else {
-            if (index != 0)
-                index = lps[index - 1];
-            else
-                lps[i] = index, i++;
+            if (index != 0) index = lps[index - 1];
+            else lps[i] = index, i++;
         }
     }
     return lps;
 }
 
 void KMP (string text, string pattern) {
-    bool found = false;
     vector<int> lps = lpsArray(pattern);
     int j = 0, i = 0;
     while (i < text.length()) {
@@ -29,10 +25,8 @@ void KMP (string text, string pattern) {
         if (j == pattern.length()) {
             cout << "found match at : " << (i - pattern.length()) << endl;
             j = lps[j-1];
-            found = true;
         }
     }
-    if (!found) cout << "not found" << endl;
 }
 
 int main() {
